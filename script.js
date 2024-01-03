fec7bcd1b8ef0c421c5b81315e84e6cb3a37415e
Ignacio Manuel Centeno <ignacenteno46@gmail.com>
2024-01-03 19:53:40 -0300

    08

--- script.js
+++ script.js
@@ -7,33 +7,17 @@ function onYouTubeIframeAPIReady() {
         width: '50%', // Ajusta el ancho según tus preferencias
         videoId: 'VW9c1EtThbM', // Reemplaza con el ID de tu video de YouTube
         playerVars: {
+            'autoplay': 1,
             'controls': 0,
             'loop': 1,
             'modestbranding': 1,
-            'start': 0,
             'mute': 0,
             'iv_load_policy': 3
         },
         events: {
-            'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
         }
     });
-}
-
-function onPlayerReady(event) {
-    // Inicia el video automáticamente al abrir la página
-    event.target.playVideo();
-
-    // Intenta poner el video en pantalla completa
-    const playerElement = document.getElementById('youtube-player');
-    if (playerElement.requestFullscreen) {
-        playerElement.requestFullscreen();
-    } else if (playerElement.mozRequestFullScreen) { // Firefox
-        playerElement.mozRequestFullScreen();
-    } else if (playerElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
-        playerElement.webkitRequestFullscreen();
-    }
 }
 
 function onPlayerStateChange(event) {
@@ -42,13 +26,16 @@ function onPlayerStateChange(event) {
         player.seekTo(0);
         player.playVideo();
     }
-}
 
-// Agrega un escucha de eventos para el evento 'fullscreenchange'
-document.addEventListener('fullscreenchange', () => {
-    // Verifica si el documento está en modo de pantalla completa
-    if (!document.fullscreenElement) {
-        // Cuando se sale del modo de pantalla completa, establece la opacidad del contenedor de fondo en 0.5
-        document.querySelector('.background-container').style.opacity = 0.5;
+    if (event.data === YT.PlayerState.PLAYING) {
+        // Intenta poner el video en pantalla completa
+        const playerElement = document.getElementById('youtube-player');
+        if (playerElement.requestFullscreen) {
+            playerElement.requestFullscreen();
+        } else if (playerElement.mozRequestFullScreen) { // Firefox
+            playerElement.mozRequestFullScreen();
+        } else if (playerElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            playerElement.webkitRequestFullscreen();
+        }
     }
-});
+}
