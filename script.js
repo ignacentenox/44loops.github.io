cccf282e65f58d4e0613ed83d202476365f93ede
Ignacio Manuel Centeno <ignacenteno46@gmail.com>
2024-01-03 20:07:35 -0300

    09

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-// JavaScript (script.js)
-var player;
-
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-player', {
-        height: '50%', // Ajusta la altura según tus preferencias
-        width: '50%', // Ajusta el ancho según tus preferencias
-        videoId: 'VW9c1EtThbM', // Reemplaza con el ID de tu video de YouTube
+        height: '50%',
+        width: '50%',
+        videoId: 'VW9c1EtThbM',
         playerVars: {
-            'autoplay': 1,
+            'autoplay': 0, // No reproduce automáticamente al cargar
             'controls': 0,
             'loop': 1,
             'modestbranding': 1,
-            'mute': 0,
+            'mute': 1, // Silencia el video al iniciar automáticamente
             'iv_load_policy': 3
         },
         events: {
             'onStateChange': onPlayerStateChange
         }
     });
-}
 
-function onPlayerStateChange(event) {
-    if (event.data === YT.PlayerState.ENDED) {
-        // Cuando el video ha terminado, reinicia la reproducción en bucle
-        player.seekTo(0);
+    // Añade un evento de clic al documento para iniciar la reproducción y pantalla completa
+    document.addEventListener('click', function () {
         player.playVideo();
-    }
-
-    if (event.data === YT.PlayerState.PLAYING) {
-        // Intenta poner el video en pantalla completa
-        const playerElement = document.getElementById('youtube-player');
-        if (playerElement.requestFullscreen) {
-            playerElement.requestFullscreen();
-        } else if (playerElement.mozRequestFullScreen) { // Firefox
-            playerElement.mozRequestFullScreen();
-        } else if (playerElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
-            playerElement.webkitRequestFullscreen();
-        }
-    }
+        player.unMute(); // Activa el sonido
+        player.setLoop(true); // Establece el bucle
+        player.webkitEnterFullscreen();
+    });
 }
+
+// ...
