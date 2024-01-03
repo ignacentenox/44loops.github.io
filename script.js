3df6aa1dae6552c5f65535596fa8fa02f9f11692
Ignacio Manuel Centeno <ignacenteno46@gmail.com>
2024-01-03 19:17:45 -0300

    up js

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ var player;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-player', {
-        height: '100%',
-        width: '100%',
+        height: '50%', // Ajusta la altura según tus preferencias
+        width: '50%', // Ajusta el ancho según tus preferencias
         videoId: 'VW9c1EtThbM', // Reemplaza con el ID de tu video de YouTube
         playerVars: {
-            'autoplay': 1,  // Reproducir automáticamente al cargar
             'controls': 0,
             'loop': 1,
             'modestbranding': 1,
@@ -16,18 +15,29 @@ function onYouTubeIframeAPIReady() {
             'iv_load_policy': 3
         },
         events: {
-            'onReady': onPlayerReady
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
         }
     });
 }
 
 function onPlayerReady(event) {
-    event.target.playVideo();
-    event.target.setVolume(100);
-    event.target.setLoop(true);
-    event.target.unMute(); // Activa el sonido
-    event.target.playVideo();
-    event.target.webkitEnterFullscreen();
+    // Espera 10 segundos y luego activa el modo de pantalla completa
+    setTimeout(() => {
+        event.target.playVideo();
+        event.target.setVolume(100);
+        event.target.unMute(); // Activa el sonido
+        event.target.playVideo();
+        event.target.webkitEnterFullscreen();
+    }, 10000); // 10 segundos en milisegundos
+}
+
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        // Cuando el video ha terminado, reinicia la reproducción en bucle
+        player.seekTo(0);
+        player.playVideo();
+    }
 }
 
 // Agrega un escucha de eventos para el evento 'fullscreenchange'
