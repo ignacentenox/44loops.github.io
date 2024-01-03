4b5f7c0b6051d605b7e58f318f852d18b294e662
Ignacio Manuel Centeno <ignacenteno46@gmail.com>
2024-01-03 20:09:47 -0300

    10

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
+// JavaScript (script.js)
+var player;
+
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-player', {
         height: '50%',
         width: '50%',
         videoId: 'VW9c1EtThbM',
         playerVars: {
-            'autoplay': 0, // No reproduce automáticamente al cargar
+            'autoplay': 1,
             'controls': 0,
             'loop': 1,
             'modestbranding': 1,
-            'mute': 1, // Silencia el video al iniciar automáticamente
+            'mute': 1,
             'iv_load_policy': 3
         },
         events: {
             'onStateChange': onPlayerStateChange
         }
     });
-
-    // Añade un evento de clic al documento para iniciar la reproducción y pantalla completa
-    document.addEventListener('click', function () {
-        player.playVideo();
-        player.unMute(); // Activa el sonido
-        player.setLoop(true); // Establece el bucle
-        player.webkitEnterFullscreen();
-    });
 }
 
-// ...
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        // Cuando el video ha terminado, reinicia la reproducción en bucle
+        player.seekTo(0);
+        player.playVideo();
+    }
+
+    if (event.data === YT.PlayerState.PLAYING) {
+        // Intenta poner el video en pantalla completa
+        const playerElement = document.getElementById('youtube-player');
+        if (playerElement.requestFullscreen) {
+            playerElement.requestFullscreen();
+        } else if (playerElement.mozRequestFullScreen) { // Firefox
+            playerElement.mozRequestFullScreen();
+        } else if (playerElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            playerElement.webkitRequestFullscreen();
+        }
+    }
+}
