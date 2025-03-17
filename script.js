ad4f67478894c727c08cc4214f2c2ca8227e339a
Ignace <ignacenteno46@gmail.com>
2025-03-17 12:11:33 -0300

    Lunes 17

--- script.js
+++ script.js
@@ -2,36 +2,38 @@
 var player;
 
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('youtube-player', {
-        height: '100%',
-        width: '100%',
-        videoId: 'PLUYzBUz0Pt6e_A94BFsCqn3PAu3-sRH2O',
-        playerVars: {
-            'autoplay': 1,
-            'controls': 0,
-            'loop': 1,
-            'modestbranding': 1,
-            'mute': 1,
-            'iv_load_policy': 3
-        },
-        events: {
-            'onStateChange': onPlayerStateChange
-        }
-    });
+    try {
+        player = new YT.Player('youtube-player', {
+            height: '100%',
+            width: '100%',
+            playerVars: {
+                'autoplay': 1,
+                'controls': 0,
+                'loop': 1,
+                'modestbranding': 1,
+                'mute': 1,
+                'iv_load_policy': 3,
+                'listType': 'playlist',
+                'list': 'PLUYzBUz0Pt6e_A94BFsCqn3PAu3-sRH2O' // ID de la playlist
+            },
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    } catch (error) {
+        console.error('Error al inicializar el reproductor de YouTube:', error);
+    }
 }
 
 function onPlayerStateChange(event) {
     if (event.data === YT.PlayerState.ENDED) {
-        // Cuando el video ha terminado, reinicia la reproducción en bucle
         player.seekTo(0);
         player.playVideo();
     }
 
     if (event.data === YT.PlayerState.PLAYING) {
-        // Después de iniciar la reproducción, activa el sonido
         player.unMute();
 
-        // Intenta poner el video en pantalla completa
         const playerElement = document.getElementById('youtube-player');
         if (playerElement.requestFullscreen) {
             playerElement.requestFullscreen();
